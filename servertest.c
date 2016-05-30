131119960e6aef3b909da3c4ae6cb4c0fbdb68f2
AnthonyManis <anthonygmanis@gmail.com>
2016-05-29 18:12:10 -0700

    added servertest.c

---
+++ servertest.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netdb.h>
+#include "csapp.h"
+
